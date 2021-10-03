ce1229bf3c470db1fd7fa8c825cf9780c8cbcbf0
Gabriel Catani <gabrielm.catani@gmail.com>
2021-10-03 20:01:18 -0300

    [UPDATE] set up final main, with TODO indications to what is needed on the main flow to finish up the project.

--- src/main.c
+++ src/main.c
@@ -1,17 +1,25 @@
 #include "my_curl.h"
 
 //$> my_curl "www.whatever.com"
-int main(void)
+int main(int argc, char **argv)
 {
   parsed_url *purl = purl_initializer();
+  if (argc != 2) {
+    my_putstr(argv[0], 0);
+    my_putstr(" :usage [url]", 1);
+    return 1;
+  }
+  //TODO: replace stirng literal with argv[1]
   purl = parse_url("http:www.google.com");
-  //Test open_connection
+  
   struct sockaddr_in *socket_info = NULL;
   http_response *http_res = NULL;
   int sockfd = 0;
+
+  //TODO: Form Http request header based on parsed user input
+  //TODO: free Parser struct, and http_header, after use
   char *http_header = "GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n";
   socket_info = set_socket(get_host_info("www.google.com"));
-
   sockfd = open_connection(socket_info);
   if (sockfd > 2)
   {
@@ -20,13 +28,6 @@ int main(void)
     get_response_and_show(sockfd, http_res);
     destroy_http_response(&http_res);
   }
-  close_connection(socket_info, sockfd);
-
-  sockfd = 0;
-  socket_info = set_socket(get_host_info("not_valid_URL"));
-
-  sockfd = open_connection(socket_info);
-
   close_connection(socket_info, sockfd);
 
   return 0;
